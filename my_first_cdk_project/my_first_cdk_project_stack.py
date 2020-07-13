0c44bc652b33aaed4a422c452e7378abf57e795e
SatyaHarish9 <harishgvt@gmail.com>
2020-07-13 12:32:00 -0500

    adding IAM group

--- my_first_cdk_project/my_first_cdk_project_stack.py
+++ my_first_cdk_project/my_first_cdk_project_stack.py
@@ -1,5 +1,6 @@
 from aws_cdk import (
     aws_s3 as _s3,
+    aws_iam as _iam,
     core
 )
 
@@ -23,6 +24,10 @@ class MyFirstCdkProjectStack(core.Stack):
             self,
             "myBucketId1"
         )
+
+        print(mybucket.bucket_name)
+
+        _iam.Group(self,"gid")
 
         output_1 = core.CfnOutput(
             self,
